68881aa6ec2aca4dba26951a4a195cb7624cd7a3
user <chynara.mk4@gmail.com>
2022-05-15 01:06:36 +0600

    smth added

--- src/main/java/peaksoft/config/AppConfig.java
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -2,8 +2,12 @@ package peaksoft.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import peaksoft.model.Timer;
 
 @Configuration
 @ComponentScan(basePackages = "peaksoft")
 public class AppConfig {
+
+
+
 }
